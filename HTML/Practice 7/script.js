952dbec374386b89245bac5e4d5f95e375330aaa
Mitesh Chaudhari <miteshchaudhari931@gmail.com>
2024-01-19 20:26:27 +0530

    slight changes

--- HTML/Practice 7/script.js
+++ HTML/Practice 7/script.js
@@ -1,3 +1,24 @@
+// Day
+(() => {
+    let day_selected = (new Date).getDate(); // current day
+    let option = '';
+    let day = '';
+    option = '<option value="">Day</option>'; // first option
+
+    for (let i = 1; i < 32; i++) {
+        // value day number adding 0. 01 02 03 04..
+        day = (i <= 9) ? '0' + i : i;
+
+        // or value day number 1 2 3 4..
+        // day = i;
+
+        let selected = (i === day_selected ? ' selected' : '');
+        option += '<option value="' + day + '"' + selected + '>' + day + '</option>';
+    }
+    document.getElementById("day").innerHTML = option;
+})();
+
+// year
 var yearSelect = document.getElementById("year");
 var currentYear = new Date().getFullYear();
 for (var i = currentYear; i >= 1900; i--) {
@@ -7,3 +28,8 @@ for (var i = currentYear; i >= 1900; i--) {
     yearSelect.add(option);
 }
 
+function handleSubmit() {
+    setTimeout(function () {
+        window.location.href = 'submit.html';
+    }, 2000);
+}
