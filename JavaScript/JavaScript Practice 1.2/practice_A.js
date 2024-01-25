8354d0e837f0bee23b385c04db68e56e45a21952
Mitesh Chaudhari <miteshchaudhari931@gmail.com>
2024-01-25 10:26:23 +0530

    File added to Git

---
+++ JavaScript/JavaScript Practice 1.2/practice_A.js
@@ -0,0 +1,15 @@
+// Create an object to store information about a person (e.g., name, age, address, etc.). 
+// Write code to access and modify the object's properties.
+
+const data = {
+    name: "Mitesh Chaudhari",
+    age: 22,
+    address: "Kalol",
+    bloodGroup: "B+",
+    codeLangauge: "Java"
+};
+
+data.address = "Ahmedabad";
+data.codeLangauge = "JavaScript";
+
+console.log(data);
