6a1bb5378acbfff731d1f1612dee5fd2fb48b6fa
Mitesh Chaudhari <miteshchaudhari931@gmail.com>
2024-02-01 17:01:05 +0530

    Modified

--- Evaluation Test/js/loginMain.js
+++ Evaluation Test/js/loginMain.js
@@ -2,18 +2,45 @@ document.addEventListener('DOMContentLoaded', function() {
 
     document.querySelector('#loginButton').addEventListener('click', () => {
 
-            let emailValue = document.getElementById('email').value;
-            let loginPwd = document.getElementById('password').value;
-
-             // Store user in local storage
-             if(localStorage.getItem('users')) {
-                var users = JSON.parse(localStorage.getItem('user'));
-                users.push(users);
-                localStorage.setItem('users', JSON.stringify(users));
-            } else {
-                localStorage.setItem('users', JSON.stringify([users]));
+            // Checking if local storage has users stored
+            if(localStorage.getItem('users')) {
+                renderUsers();
             }
+            
+            // Form submission
+            $('#loginForm').submit(function(event) {
+                event.preventDefault();
+                
+                // Get form values
+                var emailValue = $('#email').val();
+                var loginPwd = $('#password').val();
 
+                // Basic validation
+                if(emailValue && loginPwd) {
+                    // Create user object
+                    var user = {
+                        emailValue: emailValue,
+                        loginPwd: loginPwd
+                    };
+
+                    // Store user in local storage
+                    if(localStorage.getItem('users')) {
+                        var users = JSON.parse(localStorage.getItem('users'));
+                        users.push(user);
+                        localStorage.setItem('users', JSON.stringify(users));
+                    } else {
+                        localStorage.setItem('users', JSON.stringify([user]));
+                    }
+
+                    // Clear form fields
+                    $('#loginForm')[0].reset();
+
+                    // Render users
+                    renderUsers();
+                } else {
+                    alert('All fields are required!');
+                }
+            });
     });
 
 });
