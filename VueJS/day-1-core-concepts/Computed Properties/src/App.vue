9331b616d233679fa4316b36a427335c71480c39
Mitesh Chaudhari <miteshchaudhari931@gmail.com>
2024-04-09 17:53:49 +0530

    Files added to Git

---
+++ VueJS/day-1-core-concepts/Computed Properties/src/App.vue
@@ -0,0 +1,85 @@
+<template>
+  <div>
+    <h1>Computed Properties</h1>
+    <p>Previously, we were listing book from books. But now, we are listing books from newly created filteredBooks array.</p>
+    <div v-if="showBooks">
+      <ul>
+        <!-- <li v-for="book in books" :key="book.isFav" :class="{ fav: book.isFav}">   Dynamic Classes -> { key: value } -->
+        <li v-for="book in filterBooks" :key="book.isFav" :class="{ fav: book.isFav}">   <!-- Dynamic Classes -> { key: value } -->
+          <!-- <img :src="book.img" v-bind:alt="book.title"> -->
+          <img :src="book.img" :alt="book.title">
+          <h3>{{ book.title }}</h3>
+          <p>{{ book.author }}</p>
+        </li>
+      </ul>
+    </div>
+
+    <button @click="toggleShowBooks">
+      <span v-if="showBooks">Hide Books</span>
+      <span v-else>Show Books</span>
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      showBooks: true,
+      books: [
+        { title: 'name of the wind', author: 'patrick rothfuss', img: 'assets/1.jpg', isFav: true },
+        { title: 'the way of kings', author: 'brandon sanderson', img: 'assets/2.jpg', isFav: false },
+        { title: 'the final empire', author: 'brandon sanderson', img: 'assets/3.jpg', isFav: true }
+      ]
+    }
+  },
+
+  methods: {
+    toggleShowBooks() {
+      this.showBooks = !this.showBooks;
+    }
+  },
+
+  computed: {
+    filterBooks() {
+      return this.books.filter((book) => book.isFav)
+    }
+  }
+}
+</script>
+
+<style>
+body {
+  background: #eee;
+  max-width: 960px;
+  margin: 20px auto;
+}
+
+p,
+h3,
+ul {
+  margin: 0;
+  padding: 0;
+}
+
+li {
+  list-style-type: none;
+  background: #fff;
+  margin: 20px auto;
+  padding: 10px 20px;
+  border-radius: 10px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
+
+li.fav {
+            background: #ff9ed2;
+            color: white;
+        }
+
+h1, p {
+  text-align: center;
+  font-family: Arial, Helvetica, sans-serif;
+}
+</style>
---
+++ VueJS/day-1-core-concepts/Watch Properties/src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <h1>Volume Tracker (0 - 20)</h1>
+  <h3>Current Volume - {{ volume }}</h3>
+  <div>
+    <button @click="volume += 2">Increase</button>&nbsp;
+    <button @click="volume -= 2">Decrease</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  data() {
+    return {
+      volume: 0
+    }
+  },
+  methods: {},
+  computed: {},
+  watch: {
+    volume(newValue, oldValue) {
+      if(newValue > oldValue && newValue === 16) {
+        alert('Listening to a high volume for a long time may damage your hearing')
+      }
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Arial, Helvetica, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
