6e9c68d2da3af3cec777d0d62ca07991f10ec436
Mitesh Chaudhari <miteshchaudhari931@gmail.com>
2024-04-09 19:19:02 +0530

    implemented immediate & deep watcher

--- VueJS/day-1-core-concepts/Watch Properties/src/App.vue
+++ VueJS/day-1-core-concepts/Watch Properties/src/App.vue
@@ -5,6 +5,13 @@
     <button @click="volume += 2">Increase</button>&nbsp;
     <button @click="volume -= 2">Decrease</button>
   </div>
+  <input type="text" v-model="movie">
+  <input type="text" v-model="movieInfo.title">
+  <input type="text" v-model="movieInfo.actor">
+  <div>
+    <!-- <button @click="movieList.push('Wonder woman')">Add Movie</button> -->
+    <button @click="movieList = movieList.concat(['Wonder woman'])">Add Movie</button>
+  </div>
 </template>
 
 <script>
@@ -12,7 +19,13 @@ export default {
   name: 'App',
   data() {
     return {
-      volume: 0
+      volume: 0,
+      movie: 'Batman',
+      movieInfo: {
+        title: '',
+        actor: ''
+      },
+      movieList: ['Batman', 'Superman']
     }
   },
   methods: {},
@@ -22,6 +35,25 @@ export default {
       if(newValue > oldValue && newValue === 16) {
         alert('Listening to a high volume for a long time may damage your hearing')
       }
+    },
+    movie: {
+      handler(newValue) {
+        console.log(`Calling API with movie name = ${newValue}`);
+      },
+      immediate: true   // handler fun. should be run when when pageload.
+      //often with API when fetching data, it should show data when page is loaded. 
+    },
+    movieInfo: {
+      handler(newValue){
+        console.log(`Calling API withk movie title = ${newValue.title} and actor = ${newValue.actor}`);
+      },
+      deep: true
+    },
+    movieList: {
+      handler(newValue) {
+        console.log(`Updated List ${newValue}`)
+      },
+      // deep: true
     }
   }
 }
