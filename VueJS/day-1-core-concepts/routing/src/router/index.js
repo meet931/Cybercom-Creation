1ff39c62d4b807af5e96a701599e6d727ebf9647
Mitesh Chaudhari <miteshchaudhari931@gmail.com>
2024-04-10 13:41:47 +0530

    did routing core-concepts

--- VueJS/day-1-core-concepts/routing/src/router/index.js
+++ VueJS/day-1-core-concepts/routing/src/router/index.js
@@ -1,6 +1,9 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import AboutView from '../views/AboutView.vue'
+import Jobs from '../views/jobs/Jobs.vue'
+import JobDetails from '../views/jobs/JobDetails.vue'
+import NotFound from '@/views/NotFound.vue'
 
 const routes = [
   {
@@ -12,7 +15,31 @@ const routes = [
     path: '/about',
     name: 'about',
     component: AboutView
-  }
+  },
+  {
+    path: '/jobs',
+    name: 'jobs',
+    component: Jobs
+  },
+  {
+    path: '/jobs/:id',   // jobs/:id -> id is a route parameter
+    name: 'JobDetails',
+    component: JobDetails,
+    props: true
+  },
+  
+  // redirect
+  {
+    path: '/all-jobs',
+    redirect: '/jobs'
+  },
+
+  // catchall 404
+  {
+    path: '/:catchAll(.*)',
+    name: 'NotFound',
+    component: NotFound
+  },
 ]
 
 const router = createRouter({
